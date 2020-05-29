424622f6fecbff400ec0cc9047bcd397877079ab
Miguel Angel <miguelapb35@gmail.com>
2020-05-28 21:52:49 -0500

    second commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,10 +10,13 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import './bootstrap.min.css';
-import "./layout.css"
+import "./layout.css";
+import Navbar from './Globals/Navbar';
+
 
 const Layout = ({ children }) => (
   <>
+    <Navbar />
     {children}
   </>
 )
